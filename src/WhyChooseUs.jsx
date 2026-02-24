4a40d699636f66445643af3af6ece6377aafc7a7
afekhaijoseph <46360244+afekhaijoseph@users.noreply.github.com>
2026-02-24 13:24:23 +0100

    adjust why choose us

--- src/WhyChooseUs.jsx
+++ src/WhyChooseUs.jsx
@@ -10,23 +10,23 @@ const WhyChooseUs = () => {
             <img className='rounded-3xl' src={containers} alt="why choose us image" />
           </div>
           <div className='lg:basis-full w-full relative'>
-            <div className='w-full py-4 px-10 bg-accent rounded-2xl absolute top-1/2 md:top-1 lg:top-1/2 -translate-y-1/2 lg:-left-10'>
+            <div className='w-full py-1 px-10 bg-accent rounded-2xl absolute top-1/2 md:top-1 lg:top-1/2 -translate-y-1/2 lg:-left-10'>
             <Subheading variant='secondary'>Why Choose Us</Subheading>    
             <h2 className='text-4xl font-bold font-serif my-4'>Global Buyers Trust Demox</h2>
             <div>
-                <div className='flex gap-2 mb-2 items-center'>
+                <div className='flex gap-2 mb-3 items-center'>
                     <Globe size='35' className='bg-white rounded-full p-1 border-2 border-secondAccent'/>
                     <p>Global sourcing network</p>
                 </div>
-                <div className='flex gap-2 mb-2 items-center'>
+                <div className='flex gap-2 mb-3 items-center'>
                     <Cog size='35' className='bg-white rounded-full p-1 border-2 border-secondAccent'/>
                     <p>Strict quality control</p>
                 </div>
-                <div className='flex gap-2 mb-2 items-center'>
+                <div className='flex gap-2 mb-3 items-center'>
                     <ScrollText size='35' className='bg-white rounded-full p-1 border-2 border-secondAccent'/>
                     <p>Export documentation expertise</p>
                 </div>
-                <div className='flex gap-2 mb-2 items-center'>
+                <div className='flex gap-2 mb-3 items-center'>
                     <Truck size='35' className='bg-white rounded-full p-1 border-2 border-secondAccent'/>
                     <p>Global sourcing network</p>
                 </div>
